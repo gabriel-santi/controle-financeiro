60943a90de2b8275ae1d2cff1e18a2e38f8870ce
GabrielSantiago <marcosgabrielss23@gmail.com>
2024-03-23 10:57:18 -0300

    Creating Entity class and updating Transaction class

--- lib/domain/transaction.dart
+++ lib/domain/transaction.dart
@@ -1,19 +1,25 @@
-class Transaction {
-  final int? id;
+import 'package:finapp/domain/entity.dart';
+
+class Transaction extends Entity {
+  final int _id;
   final String description;
-  final DateTime date;
+  final DateTime creation;
+  final DateTime? lastUpdate;
   final double value;
 
-  Transaction._(this.id, this.description, this.date, this.value);
+  Transaction._(this._id, this.description, this.creation, this.lastUpdate, this.value);
 
-  factory Transaction.load(int id, String description, DateTime date, double value) {
-    return Transaction._(id, description, date, value);
+  @override
+  int get id => _id;
+
+  factory Transaction.load(int id, String description, DateTime creation, DateTime? lastUpdate, double value) {
+    return Transaction._(id, description, creation, lastUpdate, value);
   }
 
   factory Transaction.create(String? description, double? value) {
     _validateDescription(description);
     _validateValue(value);
-    return Transaction._(null, description!, DateTime.now(), value!);
+    return Transaction._(-1, description!, DateTime.now(), null, value!);
   }
 
   static void _validateDescription(String? description) {
@@ -22,6 +28,16 @@ class Transaction {
 
   static void _validateValue(double? value) {
     if (value == null || value <= 0) throw InvalidValue();
+  }
+
+  Transaction copyWith([String? description, DateTime? creation, DateTime? lastUpdate, double? value]) {
+    return Transaction.load(
+      _id,
+      description ?? this.description,
+      creation ?? this.creation,
+      lastUpdate ?? this.lastUpdate,
+      value ?? this.value,
+    );
   }
 }
 
