709c71826e51895402f9a98ef9809dd78ea0da6c
GabrielSantiago <marcosgabrielss23@gmail.com>
2024-03-23 09:45:24 -0300

    Fixing the color of progress bar's background

--- lib/interfaces/widget/progress_bar.widget.dart
+++ lib/interfaces/widget/progress_bar.widget.dart
@@ -3,17 +3,8 @@ import 'package:flutter/material.dart';
 class ProgressBarWidget extends StatelessWidget {
   final double maxValue;
   final double currentValue;
-  const ProgressBarWidget({super.key, required this.maxValue, required this.currentValue});
 
-  Color getColorByStatus(BuildContext context) {
-    double percentageSpent = currentValue / maxValue;
-    if (percentageSpent >= 0.7) {
-      return Theme.of(context).colorScheme.error.withOpacity(.7);
-    } else if (percentageSpent >= 0.6) {
-      return Theme.of(context).colorScheme.tertiary.withOpacity(.7);
-    }
-    return Theme.of(context).colorScheme.primary.withOpacity(.3);
-  }
+  const ProgressBarWidget({super.key, required this.maxValue, required this.currentValue});
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +13,9 @@ class ProgressBarWidget extends StatelessWidget {
         ignoring: true,
         child: Slider(
           max: maxValue,
-          value: currentValue,
+          value: currentValue.clamp(0, maxValue),
           min: 0,
-          inactiveColor: getColorByStatus(context),
+          inactiveColor: Theme.of(context).colorScheme.primary.withOpacity(.3),
           onChanged: (double value) {},
         ),
       ),
