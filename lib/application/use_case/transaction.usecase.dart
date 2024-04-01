889f9180d5a2233e481f97c0a9b5869f490ee055
GabrielSantiago <marcosgabrielss23@gmail.com>
2024-03-31 22:53:34 -0300

    Creating transaction use case

---
+++ lib/application/use_case/transaction.usecase.dart
@@ -0,0 +1,31 @@
+import 'package:finapp/application/repo/transaction.repo.dart';
+import 'package:finapp/application/state/transaction.state.dart';
+import 'package:finapp/domain/payment.dart';
+import 'package:finapp/domain/transaction.dart';
+
+class TransactionUseCase {
+  final TransactionRepo _repo;
+  final TransactionState _state;
+
+  TransactionUseCase(this._repo, this._state);
+
+  Future<void> getPaymentById(int id) async {
+    _state.transactionSelected = await _repo.getPayment(id);
+  }
+
+  Future<void> getTransactions() async {
+    _state.transactions = await _repo.getTransactions();
+  }
+
+  Future<void> savePayment(Payment transaction) async {
+    await _repo.savePayment(transaction);
+  }
+
+  Future<void> deletePayment(int id) async {
+    await _repo.deletePayment(id);
+  }
+
+  void selectTransaction(Transaction? transaction) {
+    _state.transactionSelected = transaction;
+  }
+}
