2d23f9340926037c4b956ca429d9ca39e8c1d28e
GabrielSantiago <marcosgabrielss23@gmail.com>
2024-04-07 00:07:07 -0300

    Creating getter to color

--- lib/domain/category.dart
+++ lib/domain/category.dart
@@ -1,23 +1,26 @@
 import 'package:finapp/domain/entity.dart';
+import 'package:flutter/material.dart';
 
 class Category extends Entity {
   final int _id;
   final String description;
-  final String color;
+  final String _color;
 
-  Category._(this._id, this.description, this.color);
+  Category._(this._id, this.description, this._color);
 
-  Category.create(this.description, this.color) : _id = -1;
+  Category.create(this.description, this._color) : _id = -1;
 
-  Category.load(this._id, this.description, this.color);
+  Category.load(this._id, this.description, this._color);
 
   Category.fromMap(Map<String, dynamic> map)
       : _id = map['id'],
         description = map['description'],
-        color = map['color'];
+        _color = map['color'];
 
-  Map<String, dynamic> toMap() => {'id': id, 'description': description, 'color': color};
+  Map<String, dynamic> toMap() => {'id': id, 'description': description, 'color': _color};
 
   @override
   int get id => _id;
+
+  Color get color => Color(int.parse(_color));
 }
