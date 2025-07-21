78e7134476606609888ebf81e1076a1faba63aab
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-07-21 00:33:25 -0300

    Adding go router

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -3,14 +3,48 @@ import 'package:finapp/features/transaction/interfaces/add_transaction.page.dart
 import 'package:finapp/features/transaction/interfaces/edit_limit.page.dart';
 import 'package:finapp/features/transaction/interfaces/edit_transaction.page.dart';
 import 'package:finapp/features/transaction/interfaces/home.page.dart';
-import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class Routes {
-  static Map<String, Widget Function(BuildContext)> routes = {
-    '/': (_) => const HomePage(),
-    '/transaction/create': (_) => const AddTransactionPage(),
-    '/transaction/edit': (_) => const EditTransactionPage(),
-    '/limit': (_) => const EditLimitPage(),
-    '/category': (_) => const CategoryPage(),
-  };
+enum AppRoute {
+  home('home'),
+  transactionCreate('transactionCreate'),
+  transactionEdit('transactionEdit'),
+  editLimit('editLimit'),
+  category('category');
+
+  final String name;
+
+  const AppRoute(this.name);
 }
+
+final GoRouter appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      name: AppRoute.home.name,
+      builder: (context, state) => const HomePage(),
+      routes: [
+        GoRoute(
+          path: 'transaction/create',
+          name: AppRoute.transactionCreate.name,
+          builder: (context, state) => const AddTransactionPage(),
+        ),
+        GoRoute(
+          path: 'transaction/edit',
+          name: AppRoute.transactionEdit.name,
+          builder: (context, state) => const EditTransactionPage(),
+        ),
+        GoRoute(
+          path: 'limit',
+          name: AppRoute.editLimit.name,
+          builder: (context, state) => const EditLimitPage(),
+        ),
+        GoRoute(
+          path: 'category',
+          name: AppRoute.category.name,
+          builder: (context, state) => const CategoryPage(),
+        ),
+      ],
+    ),
+  ],
+);
