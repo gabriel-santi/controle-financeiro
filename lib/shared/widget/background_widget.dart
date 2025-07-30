f1716b6c27a40ecaa24843a4d762740b8c9a0464
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-07-29 22:13:51 -0300

    Removing expanded

--- lib/shared/widget/background_widget.dart
+++ lib/shared/widget/background_widget.dart
@@ -7,31 +7,29 @@ class BackgroundWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: DecoratedBox(
-        decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface),
-        child: Stack(
-          children: [
-            Positioned(
-              bottom: -200,
-              child: Container(
-                height: MediaQuery.sizeOf(context).height / 2,
-                width: MediaQuery.sizeOf(context).width,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Theme.of(context).colorScheme.primary,
-                      blurRadius: 130,
-                      spreadRadius: 20,
-                    )
-                  ],
-                ),
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Theme.of(context).colorScheme.surface),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: -200,
+            child: Container(
+              height: MediaQuery.sizeOf(context).height / 2,
+              width: MediaQuery.sizeOf(context).width,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Theme.of(context).colorScheme.primary,
+                    blurRadius: 130,
+                    spreadRadius: 20,
+                  )
+                ],
               ),
             ),
-            child
-          ],
-        ),
+          ),
+          child
+        ],
       ),
     );
   }
