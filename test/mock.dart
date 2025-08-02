981af6173030a0fff7a364e3629327091a197ef9
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-02 18:08:25 -0300

    Category mock repo

--- test/mock.dart
+++ test/mock.dart
@@ -1,4 +1,7 @@
+import 'package:finapp/features/category/data/category_repository.dart';
 import 'package:finapp/features/transaction/data/transaction_repository.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockTransactionRepository extends Mock implements TransactionRepo {}
+
+class MockCategoryRepo extends Mock implements CategoryRepo {}
