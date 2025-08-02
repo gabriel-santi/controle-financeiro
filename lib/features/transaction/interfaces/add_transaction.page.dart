1f8704d7304a50043fce27fe6e59affcd7f510b4
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-02 16:40:00 -0300

    Behavior of AddTransactionPage on success or error

--- lib/features/transaction/interfaces/add_transaction.page.dart
+++ lib/features/transaction/interfaces/add_transaction.page.dart
@@ -3,14 +3,17 @@ import 'package:finapp/features/category/interfaces/widgets/category_selector.wi
 import 'package:finapp/features/transaction/domain/monetary_value.dart';
 import 'package:finapp/features/transaction/interfaces/bloc/add_transaction_bloc.dart';
 import 'package:finapp/features/transaction/interfaces/widgets/transaction_form.widget.dart';
+import 'package:finapp/route/routes.dart';
 import 'package:finapp/shared/constants/app_sizes.dart';
 import 'package:finapp/shared/extensions/localization.dart';
 import 'package:finapp/shared/theme/theme.dart';
 import 'package:finapp/shared/widget/button/back_button.widget.dart';
 import 'package:finapp/shared/widget/button/primary_button.widget.dart';
+import 'package:finapp/shared/widget/notification.widget.dart';
 import 'package:finapp/shared/widget/text.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class AddTransactionPage extends StatefulWidget {
   const AddTransactionPage({super.key});
@@ -28,47 +31,62 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
   @override
   Widget build(BuildContext context) {
     final addTransactionBloc = BlocProvider.of<AddTransactionBloc>(context);
-    return SafeArea(
-      child: Scaffold(
-        extendBody: true,
-        body: Padding(
-          padding: const EdgeInsets.all(Sizes.p16).copyWith(bottom: 0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: Sizes.p16),
-                const BackButtonWidget(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: Sizes.p16),
-                  child: Center(
-                    child: TextWidget(text: context.translatedString.newTransaction, size: MainTheme.fontSizeLarge, weight: FontWeight.w400),
+    return BlocListener(
+      bloc: addTransactionBloc,
+      listener: (context, state) {
+        if (state is AddTransactionFailure) {
+          return showNotification("${context.translatedString.transactionSaveFailure}:${state.errorMessage}");
+        }
+        if (state is AddTransactionSuccess) {
+          showNotification(context.translatedString.transactionSaveSuccess, NotificationType.SUCCESS);
+          context.goNamed(AppRoute.home.name);
+        }
+      },
+      child: SafeArea(
+        child: Scaffold(
+          extendBody: true,
+          body: Padding(
+            padding: const EdgeInsets.all(Sizes.p16).copyWith(bottom: 0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: Sizes.p16),
+                  const BackButtonWidget(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: Sizes.p16),
+                    child: Center(
+                      child: TextWidget(text: context.translatedString.newTransaction, size: MainTheme.fontSizeLarge, weight: FontWeight.w400),
+                    ),
                   ),
-                ),
-                TransactionFormWidget(
-                  valueController: _valueController,
-                  descriptionController: _descriptionController,
-                  formatter: _formatter,
-                  formKey: _formKey,
-                ),
-                const SizedBox(height: Sizes.p32),
-                CategorySelectorWidget(),
-                const SizedBox(height: Sizes.p64),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: BlocBuilder<AddTransactionBloc, AddTransactionState>(
-                      bloc: addTransactionBloc,
-                      builder: (context, state) {
-                        final loading = state is AddTransactionLoading;
-                        return PrimaryButtonWidget(
-                          label: context.translatedString.save,
-                          loading: loading,
-                          onClick: _onSave,
-                        );
-                      }),
-                ),
-                const SizedBox(height: Sizes.p16),
-              ],
+                  TransactionFormWidget(
+                    valueController: _valueController,
+                    descriptionController: _descriptionController,
+                    formatter: _formatter,
+                    formKey: _formKey,
+                  ),
+                  const SizedBox(height: Sizes.p32),
+                  CategorySelectorWidget(),
+                  const SizedBox(height: Sizes.p64),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: BlocBuilder<AddTransactionBloc, AddTransactionState>(
+                        bloc: addTransactionBloc,
+                        builder: (context, state) {
+                          final loading = state is AddTransactionLoading;
+                          return ConstrainedBox(
+                            constraints: const BoxConstraints(maxWidth: 300),
+                            child: PrimaryButtonWidget(
+                              label: context.translatedString.save,
+                              loading: loading,
+                              onClick: _onSave,
+                            ),
+                          );
+                        }),
+                  ),
+                  const SizedBox(height: Sizes.p16),
+                ],
+              ),
             ),
           ),
         ),
