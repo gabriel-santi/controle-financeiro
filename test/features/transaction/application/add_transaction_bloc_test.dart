db6c6ba9769a774da2f5733d395d39c42ac479f5
GabrielSantiago <marcosgabrielss23@gmail.com>
2025-08-02 18:11:00 -0300

    Moving bloc tests to 'Application' directory

--- test/features/transaction/application/add_transaction_bloc_test.dart
+++ test/features/transaction/application/add_transaction_bloc_test.dart
@@ -6,7 +6,7 @@ import 'package:finapp/features/transaction/domain/payment.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-import '../../../../mock.dart';
+import '../../../mock.dart';
 
 void main() {
   late TransactionRepo repository;
